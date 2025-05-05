6ed5464183c5ff28fae88ce0ad20000bf08cb28f
Leonardo Borges Lopes <borgesleo780@gmail.com>
2025-05-05 20:30:44 -0300

    Cria a window ao inserir a primeira forma

--- Quadrado.h
+++ Quadrado.h
@@ -8,7 +8,7 @@
 class Quadrado : public ObjetoGrafico {
 public:
     // Construtor
-    Quadrado(const Ponto& ponto1, const Ponto& ponto2, const Ponto& ponto3, const Ponto& ponto4, const QColor& cor);
+    Quadrado(const Ponto& ponto1, const Ponto& ponto2, const Ponto& ponto3, const Ponto& ponto4, const QColor& cor, const QString& nome);
     ~Quadrado() override = default;
 
     // Função para desenhar
@@ -27,6 +27,9 @@ public:
     Ponto getP2() const { return p2; }
     Ponto getP3() const { return p3; }
     Ponto getP4() const { return p4; }
+    QString getNome() const {return nome;}
+
+    QString nome;
 
 private:
     Ponto p1, p2, p3, p4;
